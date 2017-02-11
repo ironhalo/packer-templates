1c59802db673afbd4081f198940e775d64a4f406
Paul Whitehead <pwhitehead@shaitan.local>
2017-02-10 22:29:46 -0700

    updating script logic

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -3,9 +3,12 @@
 set -e
 set -x
 
-date | sudo tee /etc/vagrant_box_build_time
+if [[ `echo ${PACKER_BUILDER_TYPE}` == virtualbox-iso ]]
+then
+	date | sudo tee /etc/vagrant_box_build_time
 
-mkdir -p ~vagrant/.ssh
-curl -fsSLo ~vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-chmod 700 ~vagrant/.ssh/
-chmod 600 ~vagrant/.ssh/authorized_keys
+	mkdir -p ~vagrant/.ssh
+	curl -fsSLo ~vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
+	chmod 700 ~vagrant/.ssh/
+	chmod 600 ~vagrant/.ssh/authorized_keys
+fi
--- scripts/ubuntu-16.04/cleanup.sh
+++ scripts/ubuntu-16.04/cleanup.sh
@@ -4,3 +4,9 @@ set -e
 set -x
 
 sudo apt-get clean
+
+if [[ `echo ${PACKER_BUILDER_TYPE}` == vmware-iso ]]
+then
+	sudo deluser --remove-home vagrant
+	sudo rm -rf /etc/sudoers.d/*
+fi
