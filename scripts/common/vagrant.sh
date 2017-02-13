91ae34e23b80b8fd2d95ddcbcb2423e72cf5c5db
Paul Whitehead <pwhitehead@shaitan.local>
2017-02-13 14:02:36 -0700

    moving to old network interface naming, using root to build

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -7,8 +7,14 @@ if [[ `echo ${PACKER_BUILDER_TYPE}` == virtualbox-iso ]]
 then
 	date | sudo tee /etc/vagrant_box_build_time
 
+	useradd -p JWA0y28BTQeyo -c "Vagrant User" vagrant
+
 	mkdir -p ~vagrant/.ssh
 	curl -fsSLo ~vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 	chmod 700 ~vagrant/.ssh/
 	chmod 600 ~vagrant/.ssh/authorized_keys
+
+	echo 'Defaults:vagrant !requiretty' > /etc/sudoers.d/vagrant
+	echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/vagrant
+	chmod 440 /target/etc/sudoers.d/vagrant
 fi
